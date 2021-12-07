5a54c113c5d480a92254ad081f455258cb60622e
jaredmh1995 <71681920+jaredmh1995@users.noreply.github.com>
2021-12-07 12:20:03 -0800

    Add files via upload

---
+++ lcs.h
@@ -0,0 +1,20 @@
+
+#ifndef LCS_H
+#define LCS_H
+#include <iostream>
+using namespace std;
+
+
+
+
+
+class lcs{
+
+
+
+  public:
+
+  int lcs_it(int **C, string X, string Y, int m, int n );
+
+};
+#endif
